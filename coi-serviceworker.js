236863b16b286108f530758667e6f1a5ef3f9761
tokachi269 <39192302+tokachi269@users.noreply.github.com>
2025-06-07 11:32:52 +0900

    Updates

---
+++ coi-serviceworker.js
@@ -0,0 +1,26 @@
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+self.addEventListener("fetch", (event) => {
+    const request = event.request;
+
+    event.respondWith(
+        fetch(request)
+            .then((response) => {
+                if (response.status === 0) {
+                    return response;
+                }
+
+                const newHeaders = new Headers(response.headers);
+                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+                return new Response(response.body, {
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: newHeaders,
+                });
+            })
+            .catch((e) => console.error(e))
+    );
+});
